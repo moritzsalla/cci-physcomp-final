3ca9a84e269127fc29bf6dec25a90a03b6f9a2a7
Moritz Salla <moritz.salla@hotmail.de>
2019-12-13 13:03:46 +0100

    added desc

---
+++ code/README.md
@@ -0,0 +1,13 @@
+### Light
+
+__Moritz Salla  
+CCi Msc Physcomp 2019__
+
+Experiemental interface that uses light as an interface to output tones & melodies. It can be played by a single player or as a dialog between two players.
+
+Orange = 3.5V  
+Black = GND  
+Red = 5V  
+White = A0–A3 (photoresistors)  
+Hardwired Red = 4,5,6–13 (speakers & LEDs)  
+Violet & White = SCL & SDA (ISL)
--- week-4/light_interace/main/main.ino
+++ week-4/light_interace/main/main.ino
@@ -1,3 +1,19 @@
+// LIGHT, Moritz Salla
+// CCi Phsycomp 2019 MSc
+// --------------------
+// Experiemental interface that uses light as
+// an interface to output tones & melodies.
+// It can be played by a single player or as a
+// dialog between two players
+// --------------------
+// Orange = 3.5V
+// Black = GND
+// Red = 5V
+// White = A0–A3 (photoresistors)
+// Hardwired Red = 4,5,6–13 (speakers & LEDs)
+// Violet & White = SCL & SDA (ISL)
+// --------------------
+
 void setup() {
   Serial.begin(9600);
   torch_setup();
