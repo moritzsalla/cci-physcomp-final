eae355d6321bacd4d8dd26e46e4a12c763e97c18
Moritz Salla <moritz.salla@hotmail.de>
2020-01-07 19:11:56 +0000

    minor

--- code/main/leds.ino
+++ code/main/leds.ino
@@ -1,35 +1,19 @@
-void torch_all_high () {
-  digitalWrite(led0, HIGH);
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, HIGH);
-  digitalWrite(led3, HIGH);
-  digitalWrite(led4, HIGH);
-  digitalWrite(led5, HIGH);
-  digitalWrite(led6, HIGH);
-  digitalWrite(led7, HIGH);
-}
-
-void torch_all_low () {
-  digitalWrite(led0, LOW);
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
-  digitalWrite(led3, LOW);
-  digitalWrite(led4, LOW);
-  digitalWrite(led5, LOW);
-  digitalWrite(led6, LOW);
-  digitalWrite(led7, LOW);
-}
-
-void torch_fade (int i, int t) {
-  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 1) {
-    analogWrite(i, fadeValue);
+void led_fade (int led0, int led1, int t) {
+  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue++) {
+    analogWrite(led0, fadeValue);
+    analogWrite(led1, fadeValue);
     delay(t);
-
-    if (fadeValue == 255) {
-      for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 1) {
-        analogWrite(i, fadeValue);
-        delay(t);
-      }
-    }
   }
+  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue--) {
+    analogWrite(led0, fadeValue);
+    analogWrite(led1, fadeValue);
+    delay(t);
+  }
+}
+
+void led () {
+  led_fade(ledA0, ledB0, random(10, 50));
+  led_fade(ledA1, ledB1, random(10, 50));
+  led_fade(ledA2, ledB2, random(10, 50));
+  led_fade(ledA3, ledB3, random(10, 50));
 }
--- code/main/main.ino
+++ code/main/main.ino
@@ -8,33 +8,11 @@
 *******************************/
 
 #include "pitches.h"
-#include <Wire.h>
+#include <SoftwareWire.h>
+#include "ISL29125_SoftWire.h"
 #include "SFE_ISL29125.h"
 
-// ISL ---------------------
-SFE_ISL29125 RGB_sensor;
-
-// PHOTORESISTORS ---------------------
-int photo0pin = A0;
-int photo1pin = A1;
-int photo2pin = A2;
-int photo3pin = A3;
-int photo0input, photo1input, photo2input, photoOut3input = 0;
-
-// LEDs ---------------------
-int led0 = 6;
-int led1 = 7;
-int led2 = 8;
-int led3 = 9;
-int led4 = 10;
-int led5 = 11;
-int led6 = 12;
-int led7 = 13;
-
-// SPEAKERS ---------------------
-int piezo0 = 5;
-int piezo1 = 4;
-int speed = 90;
+ISL29125_SOFT RGB_sensor;
 
 int melody[] = {
   NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
@@ -44,63 +22,59 @@ int noteDurations[] = {
   4, 8, 8, 4, 4, 4, 4, 4
 };
 
+int photo0pin = A0;
+int photo1pin = A1;
+int photo2pin = A2;
+int photo3pin = A3;
+int photo0input, photo1input, photo2input, photoOut3input = 0;
+
+int ledA0 = 6;
+int ledA1 = 8;
+int ledA2 = 11;
+int ledA3 = 12;
+int ledB0 = 7;
+int ledB1 = 13;
+int ledB2 = 9;
+int ledB3 = 10;
+
+int piezo0 = 5;
+int piezo1 = 4;
+int speed = 90;
+
 void setup() {
   Serial.begin(9600);
 
-  // ISLs ---------------------
-  if (RGB_sensor.init())
+  if (RGB_sensor.init(38,36)) 
   {
     Serial.println("Sensor Initialization Successful\n\r");
   }
-
-  // PHOTORESISTORS: SETUP ---------------------
-  pinMode(led0, OUTPUT);
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-  pinMode(led5, OUTPUT);
-  pinMode(led6, OUTPUT);
-  pinMode(led7, OUTPUT);
 }
 
 void loop() {
-  // PHOTORESISTORS ---------------------
-  int photo0in = analogRead(photo0pin);
-  int photo1in = analogRead(photo1pin);
-  int photo2in = analogRead(photo2pin);
-  int photo3in = analogRead(photo3pin);
+  // led();
 
-  int photoAvg = (photo0in + photo1in + photo2in + photo3in) / 4;
+  // PHOTORESISTOR ---------------------
+  //  int photo0in = analogRead(photo0pin);
+  //  int photo1in = analogRead(photo1pin);
+  //  int photo2in = analogRead(photo2pin);
+  //  int photo3in = analogRead(photo3pin);
+  //  int photoAvg = (photo0in + photo1in + photo2in + photo3in) / 4;
+  //  tone(piezo1, photoAvg);
 
   // ISLs ---------------------
   unsigned int red = RGB_sensor.readRed();
   unsigned int green = RGB_sensor.readGreen();
   unsigned int blue = RGB_sensor.readBlue();
 
-  // You can change HEX to DEC
-  // Serial.print("R: "); Serial.println(red,HEX);
-  // Serial.print("G: "); Serial.println(green,HEX);
-  // Serial.print("B: "); Serial.println(blue,HEX);
-  // Serial.println();
+  Serial.print("R: "); Serial.println(red, HEX);
+  Serial.print("G: "); Serial.println(green, HEX);
+  Serial.print("B: "); Serial.println(blue, HEX);
+  Serial.println();
 
-  int redAvg = (red0 + red1 + red2 + red3) / 4;
-  int green = (greenAvg0 + greenAvg1 + greenAvg2 + greenAvg3) / 4;
-  int blue = (blue0 + blue1 + blue2 + blue3) / 4;
+//  int redAvg = (red0 + red1 + red2 + red3) / 4;
+//  int green = (greenAvg0 + greenAvg1 + greenAvg2 + greenAvg3) / 4;
+//  int blue = (blue0 + blue1 + blue2 + blue3) / 4;
+//  int colorAvg = (red + green + blue) / 3;
 
-  int colorAvg = (red + green + blue) / 3;
-
-  Serial.println(colorAvg);
-
-  // LEDs ---------------------
-  // torch_fade(led0, 1);
-  // torch_fade(led1, 2);
-  // torch_fade(led2, 1);
-  // torch_fade(led3, 1);
-  // torch_fade(led4, 2);
-  // torch_fade(led5, 1);
-  // torch_fade(led6, 3);
-
-  // SPEAKERS ---------------------
-  // tone(piezo0, 20, 0.1);
+// tone(piezo1, photoAvg);
 }
